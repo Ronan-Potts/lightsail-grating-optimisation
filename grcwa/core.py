Fixed transverse force for asymmetric sail in core.py. Added efficiency derivative calculations to running_core.py

grcwa/core.py
@@ -421,7 +421,7 @@ def grcwa_efficiencies(nG,orders,cell_geometry,Nx,d,theta,freq):
 
         d: width of the unit cell.
 
-        theta: incident angle of light.
+        theta: incident angle of light in degrees.
     '''
     ## Frequency
     Qabs = np.inf
@@ -554,23 +554,26 @@ def grcwa_transverse_force(nG,cell_geometry,Nx,d,theta,freq,beta):
     This can be done in a few ways. Haven't yet decided which one is the best. Also sub-optimal as it requires
     recompiling the grcwa solver.
     '''
-    En1_fun = lambda theta: grcwa_efficiencies(nG,orders,cell_geometry,Nx,d,theta,freq)[-1]
+    En1_fun = lambda theta: grcwa_efficiencies(nG,[0,1,-1],cell_geometry,Nx,d,theta,freq)[-1]
+    E1_fun = lambda theta: grcwa_efficiencies(nG,[0,1,-1],cell_geometry,Nx,d,theta,freq)[1]
     pEn1_pTheta = grad(En1_fun)(0.)
-    # numerical_partial = (En1_fun(0.005) - En1_fun(-0.005)) / 0.01
+    pE1_pTheta = grad(E1_fun)(0.)
     
-    ## Speed of light and wavelength of the incident light
-    wavelength = c/freq
+    
 
     ## Relativistic terms
     v = beta*c
     gamma = 1/np.sqrt(1-beta**2)
-    '''
-    Not sure if this definition of D1 is entirely correct
-    '''
     D1 = np.sqrt((1-beta)/(1+beta))
 
+    ## Wavelength of the incident light
+    wavelength = D1*c/freq
+
+    Qpr1 = 2*e[0] + (e[1] + e[-1])*(1 + np.sqrt( 1 - (wavelength/d)**2 ))
+    pQpr2_pTheta = -2*e[0] - (pE1_pTheta - pEn1_pTheta)*(wavelength/d) - (e[1] + e[-1])*(1 + np.sqrt( 1 - (wavelength/d)**2 ))
+
     ## Transverse force
-    return -1*(1/v)*(D1**2)*( 2*(wavelength/d)*pEn1_pTheta*(1/D1 - 1) - (gamma-1)*(2*e[0] + 2*e[1]*(1 + np.sqrt( 1 - (wavelength/d)**2 ))) )
+    return -1*(1/v)*(D1**2)*(pQpr2_pTheta*(1/D1 - 1) + gamma*beta*Qpr1)
 
 def valid(vars):
     if len(vars) == 2:
@@ -648,6 +651,13 @@ def valid_eps(vars,d,Nx,x1,x2,w1,w2):
             w2 = w2 + d/(Nx-1)
 
         return vars,x1,x2,w1,w2
-
+    
     else:
         raise Exception("Incorrect length for input variables. You input vars of length",len(vars),", where length 2 or 4 are needed.")
+
+
+
+'''
+Objectives:
+    1) Change transverse force cost for a possibly asymmetric grating.
+'''

grcwa/optimising_force_2.0.py
@@ -6,7 +6,7 @@ import autograd.numpy as np
 from autograd import grad
 import matplotlib.pyplot as plt
 
-save_figs = False
+save_figs = True
 
 ## Discretisation values
 nG = 30
@@ -98,7 +98,7 @@ while optimising:
         grad = grad_fun(vars)
 
         # Step in direction of local minimum
-        vars = vars + step_size*np.array(grad)
+        vars = vars - step_size*np.array(grad)
 
         # Ensure that the permittivities are in range [E_vacuum, E_Si]
         while core.valid(vars) == False:
@@ -107,34 +107,24 @@ while optimising:
         # Calculate cost function after stepping
         obj = min_objective(vars)
         
+
+        # Condition 2: recent values are identical
+        if step > 5 and max(obj_vals[-2:]) - min(obj_vals[-2:]) < 1e-3:
+            step_size = 2*step_size # increase step size for next time
+            
         # Condition 1: minimum has been overstepped, leading objective to increase
         if obj - obj_vals[-1] > 1e-5:
             overstep_count += 1
             all_objs.append(obj)
             if overstep_count > 20:
                 print("\nOptimum reached after {} steps.".format(step))
-                vars = var_vals[-1] # move vars back to their original place
-                w1 = w_vals[-1][0]
-                w2 = w_vals[-1][1]
-                x1 = x_vals[-1][0]
-                x2 = x_vals[-1][1]
                 break
             else:
                 step_size = 0.9*step_size # decrease the step size
                 print("{:<13} | {:<10.5f} | {:<8.3f} | {:<8.3f} | {:<8.3f} | {:<8.3f} | {:<8.3f} | {:<8.3f}".format('OVERSTEP #{}'.format(overstep_count),obj,vars[0],vars[1],x1,x2,w1,w2), end='\r')
-                
-                vars = var_vals[-1] # move vars back to their original place
-                w1 = w_vals[-1][0]
-                w2 = w_vals[-1][1]
-                x1 = x_vals[-1][0]
-                x2 = x_vals[-1][1]
                 continue
         else:
             overstep_count = 0
-
-        # Condition 2: recent values are identical
-        if step > 5 and max(obj_vals[-2:]) - min(obj_vals[-2:]) < 1e-3:
-            step_size = 2*step_size # increase step size for next time
             
 
         # Add to history of parameters and objective values
@@ -155,6 +145,7 @@ while optimising:
 
     step += 1 # move to next step in optimisation
 
+vars = var_vals[-1]
 x1,x2 = x_vals[-1]
 w1,w2 = w_vals[-1]
 basic_geometry = core.basic_cell_geometry(Nx,d,x1,x2,w1,w2)
@@ -176,7 +167,7 @@ plt.close()
 all_objs.sort()
 steps = range(0,len(all_objs))
 
-plt.plot(steps, all_objs, 'o')
+plt.plot(steps, all_objs)
 plt.savefig('grcwa/figs/optimising_force_2.0.png')
 
 '''